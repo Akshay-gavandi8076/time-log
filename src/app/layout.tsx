49f0f3c7e18bd0999974438fc661c87be06446cf
Akshay-gavandi8076 <Akshay.gavandi1@gmail.com>
2023-12-05 01:22:24 +0100

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <main className='max-w-6xl mx-auto min-h-screen'>{children}</main>
+      </body>
     </html>
   )
 }
